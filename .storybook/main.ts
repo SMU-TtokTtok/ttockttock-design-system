41fdd65225807c8998f0e96d1c250d7d3f4be5fc
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-12-03 15:57:23 +0900

    feat: 스토리북 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,33 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const dirname =
+  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs"
-  ],
- 
-  "framework": "@storybook/react-vite"
+
+  framework: '@storybook/react-vite',
+
+  async viteFinal(config) {
+    const { mergeConfig } = await import('vite');
+
+    return mergeConfig(config, {
+      plugins: [vanillaExtractPlugin()],
+      resolve: {
+        alias: {
+          '@': path.resolve(dirname, '../src'),
+        },
+      },
+    });
+  },
 };
 export default config;
