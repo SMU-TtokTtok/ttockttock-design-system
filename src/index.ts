efe6e64132bf20f9b4ff5e11622431b2e716a20f
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-12-11 18:23:00 +0900

    refactor: 절대경로에서 상대경로로 변경

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-// colors, typography, animation
-export { vars } from '@/styles/theme.css';
-export { fadeIn, fadeInRight, fadeOutRight } from '@/styles/animation.css';
-export * from '@/styles/theme.css';
+// Design tokens (colors, typography, animation)
+export { vars } from './styles/theme.css';
+export { fadeIn, fadeInRight, fadeOutRight } from './styles/animation.css';
+export * from './styles/theme.css';
 
 // Components
-export { Button } from '@/ui/button/index';
-export { Checkbox } from '@/ui/checkbox/index';
-export { DropDownButton } from '@/ui/dropdownButton/index';
-export { Input } from '@/ui/input/index';
-export { SearchBar } from '@/ui/searchBar/index';
-export { Tag } from '@/ui/tag/index';
+export { Button } from './ui/button/index';
+export { Checkbox } from './ui/checkbox/index';
+export { DropDownButton } from './ui/dropdownButton/index';
+export { Input } from './ui/input/index';
+export { SearchBar } from './ui/searchBar/index';
+export { Tag } from './ui/tag/index';
